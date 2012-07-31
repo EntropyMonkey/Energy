1845c7c8f1f3c3a439f67925a70ea9ae9b0337b0
Andrea Distler <andrea.distler@gmail.com>
2012-07-31 18:56:45 +0200

    added first model

--- UnityProjekt/Assets/scripts/CustomCamera.cs
+++ UnityProjekt/Assets/scripts/CustomCamera.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class CustomCamera : MonoBehaviour 
 {
+	public const float speed = 100.0f;
 
-	Camera camera;
+	// the minimum mouse position at which no scrolling happens
+	public Vector2 minMousePosition = new Vector2();
+
+	// maximum mouse position at which no scrolling happens
+	public Vector2 maxMousePosition = new Vector2();
 
 	// Use this for initialization
 	void Start () 
@@ -15,9 +20,35 @@ public class CustomCamera : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown(KeyCode.DownArrow))
+		Vector3 direction = Vector3.zero;
+
+		if (Input.GetKey(KeyCode.DownArrow))
 		{
-			//Move();
+			direction = new Vector3(0, 0, -1);
+		}
+		else if (Input.GetKey(KeyCode.UpArrow))
+		{
+			direction = new Vector3(0, 0, 1);
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			direction = new Vector3(-1, 0, 0);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			direction = new Vector3(1, 0, 0);
+		}
+
+		transform.position += direction * speed * Time.deltaTime;
+
+		Debug.Log(Input.mousePosition);
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(10, 10, 150, 100), "I am a button"))
+		{
+			print("You clicked the button!");
 		}
 	}
 }
