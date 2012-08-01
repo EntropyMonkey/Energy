36a0e3c1e30c6bfac6195f8e44b4439274e461b0
unknown <daniritz12@Client2.camps.local>
2012-08-01 10:53:27 +0200

    BuildingGeruest

--- UnityProjekt/Assets/scripts/Building.cs
+++ UnityProjekt/Assets/scripts/Building.cs
@@ -2,7 +2,19 @@ using UnityEngine;
 using System.Collections;
 
 public class Building : MonoBehaviour {
-
+	
+	protected Tile tileRef;
+	protected bool enabled;
+	protected Dictionary<ResourceType, float> Input = 
+            new Dictionary<string, float>();
+	protected Dictionary<ResourceType, float> Output = 
+            new Dictionary<string, float>();
+	protected Dictionary<ResourceType, float> CurrentInput = 
+            new Dictionary<string, float>();
+	protected Dictionary<ResourceType, float> CurrentOutput = 
+            new Dictionary<string, float>();
+	protected List<Upgrade> Upgrades;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +23,16 @@ public class Building : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		
+	}
+	
+	public void updatePollution(){
+	}
+	public float updateEfficiency(){
+	}
+	public void updateOutput(){
+		Output = Output * Efficiency;
+	}
+	public void updateInput(){
 	}
 }
