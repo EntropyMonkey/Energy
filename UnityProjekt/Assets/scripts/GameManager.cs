e581c69100270dd3146a2a566aeba54e30f4925a
campnerd <camnerd@mailinator.com>
2012-08-02 12:01:08 +0200

    bug fix

--- UnityProjekt/Assets/scripts/GameManager.cs
+++ UnityProjekt/Assets/scripts/GameManager.cs
@@ -26,7 +26,7 @@ public class GameManager : MonoBehaviour
 	//TODO add UI
 
 	public List<GameObject> prefabs;
-	public List<XmlParser.Building> Buildings;
+	public List<XMLParser.Building> Buildings;
 	
 	//TODO add map
 
@@ -34,7 +34,8 @@ public class GameManager : MonoBehaviour
 	void Start () 
 	{
 		prefabs = new List<GameObject>();
-		Buildings = new List<Building>();
+		Buildings = new List<XMLParser.Building>();
+		
 		//read game values xml data
 		Parser parser = new Parser("gamevalues.xml");
 		Buildings = parser.Buildings;
